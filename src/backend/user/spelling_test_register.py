98ee5ca0ce9e28c2b9c5650fe806d873b4aea66d
Jacob Nettleship <jpnettleship@gmail.com>
2021-11-05 19:39:26 +0000

    add database, test, leaderboard subpackages

--- src/backend/user/spelling_test_register.py
+++ src/backend/user/spelling_test_register.py
@@ -4,5 +4,12 @@
 from src.backend.database.spelling_test_database import insert
 
 
-def register(username, password, email):
+def register(username: str, password: str, email: str):
+    """
+    This file verifies the validity
+    :param username:
+    :param password:
+    :param email:
+    :return:
+    """
     insert("Users", username, password, email)
