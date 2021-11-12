b98d5c841e28d6834aaba1b364d8d59d25d1bb79
WaffleOnGithub <93714578+WaffleOnGithub@users.noreply.github.com>
2021-11-12 10:50:30 +0000

    Update __init__.py
    got rid of. unnecessary stuff

--- src/backend/test/__init__.py
+++ src/backend/test/__init__.py
@@ -1,12 +1,1 @@
-difficulties = {
-    "easy": ["coming", "inside", "parents", "guard", "lamb", "wrap", "geese", "gem", "touch", "wreck", "foul",
-             "lure", "chief", "gnat", "gnaw", "quip", "raise", "sauce", "thigh", "balm", "calm", "weigh", "Zeus",
-             "column", "zoology", "corpses", "cities", "passage", "including", "compartment"],
-    "medium": [],
-    "hard": []
-}
 
-if __name__ == "__main__":
-    print(len(difficulties["easy"]))
-    print(len(difficulties["medium"]))
-    print(len(difficulties["hard"]))
