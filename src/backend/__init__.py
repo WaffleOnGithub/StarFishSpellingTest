406675951a618f994bcb690f12e54823aacd7463
Joe <92203324+joe-does-things-here@users.noreply.github.com>
2021-11-22 19:18:33 +0000

    email code
    added the email code and updated the __init__ file to import the code

--- src/backend/__init__.py
+++ src/backend/__init__.py
@@ -1,6 +1,6 @@
 """
-Edited by: Jacob
-Date edited: 12/11/21
+Edited by: Joe
+Date edited: 22/11/21
 
 Init file for backend package
 This is a space to import things that should be accessible when importing backend
@@ -9,3 +9,4 @@ This is a space to import things that should be accessible when importing backen
 from .user import *
 from .test import *
 from .leaderboard import *
+from .email import*
---
+++ src/backend/email/spelling_test_email.py
@@ -0,0 +1,34 @@
+"""
+Edited by: Joe
+Date edited: 22/11/21
+function for creating,sending and returning a randomly generated 2 factor authenication code
+"""
+import smtplib, ssl,random,string
+#imports necessary libraries
+
+def send_email(user_email):
+    port = 465
+    password = "ietyxvyiedswudze"
+    sender_email="userverifaction@gmail.com"
+    receiver_email=user_email
+    code=""
+    message = """\
+Subject: Spelling Test User Verifacation
+
+Your two step verifacation code is """""
+    
+    for i in range(6):
+        operation = random.randint(0,1)
+        if operation == 0:
+            code = code+str(random.randint(0,9))
+        else:
+            code = code+random.choice(string.ascii_letters)
+    message = message+code
+    context = ssl.create_default_context()
+    # Create a secure SSL context
+
+    with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
+        server.login("userverifaction@gmail.com", password)
+        server.sendmail(sender_email, receiver_email, message)
+        return(code)
+
